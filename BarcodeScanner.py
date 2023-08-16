fb44fe8d1ed1a801119d3a4de4e058bc6a147d78
dylandomingo57 <dylandomingo57@gmail.com>
2023-08-16 12:53:34 -1000

    testing

--- BarcodeScanner.py
+++ BarcodeScanner.py
@@ -1,46 +1,76 @@
 import csv
 import datetime
+import atexit
+
+counter = 0
 
 # print ("Current date and time = %s" % e)
 # print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
 # print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
 
 def readBarcode(code):
-    with open("SY 22-23 Student Barcodes.csv") as file:
-        csv_reader = csv.reader(file)
+    found = False
+    index = None
+    with open("SY 22-23 Student Barcodes.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
         #print("started")
         for row in csv_reader:
             #print("in row")
             for val in row:
                 #print("in val")
                 if code in val:
-                    return(val)
+                    found = True
+                    index = val
+    if (found):
+        return index
+    else:
+        return "Not found"
+
+def endProgram():
+    f = open("Cheesebread.txt", "a")
+    f.write(counter)
+    f.close()
 
 
 if __name__ == "__main__":
+    atexit.register(endProgram)
+
     print("-------------------------------\n|     Library Appender        |\n-------------------------------\n")
     print("To enter a student's name manually, enter their credentials with \"LastName, FirstName\"")
     print("i.e.\"Domingo, Dylan\"\n")
+    print("Enter \"!count\" to view the student count")
     print("Enter \"!exit\" to exit the program\n")
+
     while (True):
+
         id = input()
         name = readBarcode(id)
-
+        
         e = datetime.datetime.now()
 
-        if type(id) == None:
-            print("Not found, please try again.")
-
         if id == "!exit":
+            endProgram()
             break
 
-        f = open("Cheesebread.txt", "a")
-        f.write(name)
-        f.write("\t%s/%s/%s" % (e.month, e.day, e.year))
-        f.write("\t%s:%s:%s" % (e.hour, e.minute, e.second))
-        f.write("\n")
-        f.close()
+        if id == "!count":
+            print("Count: ", counter, "\n")
 
-        print("Student inputted!\n")
+        if name == "Not found":
+            if id !="!count":
+                print("Not found, please try again.\n")
+        else:
+            counter += 1
+
+            f = open("Cheesebread.txt", "a")
+            f.write(name)
+
+            f.write("\t%s/%s/%s" % (e.month, e.day, e.year))
+            f.write("\t%s:%s:%s" % (e.hour, e.minute, e.second))
+
+            f.write("\n")
+            f.close()
+
+            f = open("Cheesebread.txt", "r")
+            print(f.read())
     quit()
         
